cb57746cab406242939c8aba10f58b253a1c62ed
varuniit1994@gmail.com <varuniit1994@gmail.com>
2020-09-10 17:20:30 +0530

    written first component in react.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
+function FirstComponent()
+{
+  return(
+    <h2>Hello World 2</h2>
+  )
+}
+
 function App() {
   return (
-    <h1>Hello World</h1>
-  );
+    <div>
+      <h1>Hello World</h1>
+      <FirstComponent></FirstComponent>
+    </div>
+  )
 }
 
 export default App;
