722f3996c9c44ae8453788c4cfaf451fd9f8cc3c
varuniit1994@gmail.com <varuniit1994@gmail.com>
2020-09-10 16:35:18 +0530

    My first change in react project.

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>Wow this look so cool</p>
         <a
           className="App-link"
           href="https://reactjs.org"
