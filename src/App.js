42bfacbe6df1bd3509caf504ef8b105f2ee71e86
varuniit1994@gmail.com <varuniit1994@gmail.com>
2020-09-11 14:31:18 +0530

    How Children elment in propps can be used.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function GreetComponent(props)
     <div>
       <h4>{props.name}</h4>
       <h5>{props.age}</h5>
+      <p>{props.children}</p>
     </div>
   )
 }
@@ -16,7 +17,9 @@ function App()
   return(
      <div>  
         <h2>Hello World</h2>
-        <GreetComponent name="Varun_Bhati" age={20}></GreetComponent>
+        <GreetComponent name="Varun_Bhati" age={20}>
+          I am Children of GreetComponent!!!...
+        </GreetComponent>
      </div>
   )
 }
