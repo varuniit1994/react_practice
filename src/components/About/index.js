ade04cf5293f668c74b4fe3738fa52d35b190f69
varuniit1994@gmail.com <varuniit1994@gmail.com>
2020-09-14 18:22:11 +0530

    useContext Hook in React.JS

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,46 @@
-import React from "react";
+import React,{ useContext, useState, useEffect } from "react";
+
+const MyContext=React.createContext();
+
+function MyComponent()
+{
+    const { state,setState }=useContext(MyContext);
+
+    useEffect(()=>{
+
+        setTimeout(()=>{
+           
+            setState((state)=>({
+                ...state,
+                name:"updated name"
+            }))
+        },1000);
+
+    },[]);
+
+    return  (
+        <div>
+            <h1>This is a custom MyComponent  </h1>
+            <h1>Name: {state.name}</h1>
+            <h1>Age: {state.age}</h1>
+        </div>
+    )
+}
 
 export default function About()
 {
-    return <h1>About</h1>
+    const [obj, setObj]=useState({
+        name:"varun_bhati",
+        age:26,
+        prop:"random"
+    });
+
+    return (
+        <MyContext.Provider value={{state:obj,setState:setObj}}>
+          <div>
+              <MyComponent/>
+          </div>
+        </MyContext.Provider>
+    )
 }
+
