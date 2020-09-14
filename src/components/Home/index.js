e23e6c449fb51dee34b058f9f8fc48dfde739488
varuniit1994@gmail.com <varuniit1994@gmail.com>
2020-09-14 08:42:17 +0530

    use of useState and useEffect Hooks for state management.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useRef,useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Home()
 {
-    return <h1>Home GOTO <Link to="/about">About</Link></h1>
+
+    const h1ref=useRef();
+    const [counter1,setCounter1]=useState(0);
+    const [counter2,setCounter2]=useState(0);
+
+    useEffect(()=>{
+        console.log(h1ref);
+    },[counter2]);
+
+    return (
+        <div>
+            <button onClick={()=>setCounter1(counter1=>counter1+1)}>counter1: {counter1}</button>
+            <button onClick={()=>setCounter2(counter2=>counter2+1)}>counter2: {counter2}</button>
+            <h1 ref={h1ref}>Home GOTO <Link to="/about">About {counter1} {counter2}</Link></h1>
+        </div>
+        )
 }
